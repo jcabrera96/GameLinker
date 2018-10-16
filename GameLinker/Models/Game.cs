811946cc18cfa08ee86b216024d8a3e5256ce36e
LKO124 <jorge_cabreramelian@hotmail.com>
2018-10-16 16:53:29 +0100

    Created Game class

---
+++ GameLinker/Models/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLinker
+{
+
+
+    class Game
+    {
+        private string savePath, dataPath;
+
+        public Game(string savePath = null, string dataPath = null)
+        {
+            if (savePath == null && dataPath == null)
+            {
+                throw new ArgumentNullException();
+            }
+            this.savePath = savePath;
+            this.dataPath = dataPath;
+        }
+
+        public string SavePath
+        {
+            get => savePath;
+            set => savePath = value;
+        }
+
+        public string DataPath
+        {
+            get => dataPath;
+            set => dataPath = value;
+        }
+    }
+}
