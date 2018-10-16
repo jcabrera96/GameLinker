ec166b0972ceabd75d56a7003097fbefeff62e3c
LKO124 <jorge_cabreramelian@hotmail.com>
2018-10-16 17:05:15 +0100

    Created GamesLibrary class

--- GameLinker/Models/Game.cs
+++ GameLinker/Models/Game.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameLinker
 {
-
-
+    [Serializable]
     class Game
     {
         private string savePath, dataPath;
---
+++ GameLinker/Models/GamesLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GameLinker.Models
+{
+    [Serializable]
+    class GamesLibrary
+    {
+        private ArrayList library;
+        public GamesLibrary()
+        {
+            library = new ArrayList();
+        }
+        public void AddGame(Game item)
+        {
+            library.Add(item);
+        }
+        public void RemoveGame(Game item)
+        {
+            library.Remove(item);
+        }
+    }
+}
